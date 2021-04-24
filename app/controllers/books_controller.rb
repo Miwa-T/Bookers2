e9825a53aa9c5d8ca0854c6bd28a3de304596af7
Miwa-T <miwa951107@gmail.com>
2021-04-24 12:22:33 +0000

    [finish]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @newbook = Book.new
-    @user = current_user
+    @user = @book.user
   end
 
   def create
@@ -27,6 +27,11 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
 
   def update
